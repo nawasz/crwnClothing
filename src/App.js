28656e7b4ba30af7c06dd7de837fcae0b9fd3d32
Nawaz <nawaznz261970@gmail.com>
2025-04-15 06:24:17 +0530

    Setting Up Our Categories

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const categories = [
+    {
+      id:1,
+      title:'Hats'
+    },
+    {
+      id:2,
+      title:'Jackets'
+    },
+    {
+      id:3,
+      title:'SNEAKERS'
+    },
+    {
+      id:4,
+      title:'WOMENS'
+    },
+    {
+      id:5,
+      title:'MENS'
+    }
+
+  ]
+  return (
+   <div className="categories-container">
+
+{categories.map(({title}) => (<div className="category-container">
+              {/* <img/> */}
+                <div className ="category-body-container">
+                  <h2>{title}</h2>
+                  <p>Shop Now</p>
+                </div>
+          </div>))}
+          
+    
+          
+         
+          
+
+   </div>
+  );
+}
+
+export default App;
